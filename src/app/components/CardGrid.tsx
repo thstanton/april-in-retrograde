33b188369c91fd8ff53253ac34dd903bcaf4db8b
thstanton <thstanton@proton.me>
2024-02-13 12:51:30 +0000

    Card grid title added, testing font

--- src/app/components/CardGrid.tsx
+++ src/app/components/CardGrid.tsx
@@ -5,21 +5,27 @@ import PlayListCard from "./PlaylistCard";
 
 interface CardGridProps {
   links: LinkItem[];
+  title: string;
 }
 
-export default async function CardGrid({ links }: CardGridProps) {
+export default async function CardGrid({ links, title }: CardGridProps) {
   const categoryMap = await getCategories();
   return (
-    <div className="flex flex-wrap gap-8">
-      {links.map((linkItem: LinkItem) =>
-        linkItem.categoryId === categoryMap.starter ||
-        linkItem.categoryId === categoryMap.main ||
-        linkItem.categoryId === categoryMap.dessert ? (
-          <FoodCard key={linkItem.id} linkItem={linkItem} />
-        ) : (
-          <PlayListCard key={linkItem.id} linkItem={linkItem} />
-        ),
-      )}
+    <div>
+      <div className="font-display text-xl text-blue-900 border-b-blue-900 border-b-2 border-solid mb-3 pb-3">
+        <h1>{title}</h1>
+      </div>
+      <div className="flex flex-wrap gap-8">
+        {links.map((linkItem: LinkItem) =>
+          linkItem.categoryId === categoryMap.starter ||
+          linkItem.categoryId === categoryMap.main ||
+          linkItem.categoryId === categoryMap.dessert ? (
+            <FoodCard key={linkItem.id} linkItem={linkItem} />
+          ) : (
+            <PlayListCard key={linkItem.id} linkItem={linkItem} />
+          ),
+        )}
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CardGrid links={links} />
+      <CardGrid title="Starters" links={links} />
     </main>
   );
 }
