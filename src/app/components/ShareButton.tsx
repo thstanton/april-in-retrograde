7527a5696c9a49f1a9d2d9c08e3b219c1236edb5
thstanton <thstanton@proton.me>
2024-02-13 18:44:55 +0000

    Card hover state with additional icons

---
+++ src/app/components/ShareButton.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { IoShareOutline } from "react-icons/io5"
+
+
+export default function ShareButton() {
+  return (
+    <button className={`flex justify-center text-3xl`}><IoShareOutline /></button>
+  )
+}
---
+++ src/app/components/VisitLinkButton.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { FiExternalLink } from "react-icons/fi"
+
+export default function VisitLinkButton() {
+  return (
+    <button className={`flex justify-center text-3xl`}><FiExternalLink /></button>
+  )
+}
