c9efdd4324792a83fefd420b87c5eeb1e92bc384
thstanton <thstanton@proton.me>
2024-03-06 22:32:52 +0000

    Footer

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -3,7 +3,7 @@ import { cloudinaryUrl } from "../lib/cloudinary/cloudinary";
 
 export default function FeatureCard() {
   return (
-    <div className="card card-side bg-blue-900 w-full rounded-none max-h-fit">
+    <div className="card card-side mb-3 max-h-fit w-full rounded-none bg-blue-900">
       <figure className="">
         <Image
           src={`${cloudinaryUrl}https://images.unsplash.com/photo-1593034509785-5b17ba49f683?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
@@ -21,7 +21,7 @@ export default function FeatureCard() {
           libero hic illum sed odio doloribus necessitatibus.
         </p>
         <div className="card-actions">
-            <button className="btn">Show me</button>
+          <button className="btn">Show me</button>
         </div>
       </div>
     </div>
